897ce54924360506aeeb5ea422bd1bf9500143d5
Krishna Teja <krishnatejakool@gmail.com>
2024-04-04 23:37:25 +0200

    modified flask to incorporate sam prediction as well

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import subprocess
 
 app = Flask(__name__)
 
+app.config['RESULTS_FOLDER'] = './static/runs/predict-seg'
 
 @app.route("/")
 def hello_world():
-    return render_template('index_cus.html')
+    return render_template('index.html')
 
     
 @app.route("/", methods=["GET","POST"])
@@ -18,27 +19,26 @@ def predict_img():
         if 'file' in request.files:
             f = request.files['file']
             basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath,'uploads',f.filename)
+            filepath = os.path.join(basepath,'static','uploads',f.filename)
             print("upload folder is ", filepath)
             f.save(filepath)
             global imgpath
             predict_img.imgpath = f.filename
             print("printing predict_img :::::: ", predict_img)
-                                               
-            file_extension = f.filename.rsplit('.', 1)[1].lower() 
             
             cmd = ["python","./segment/predict.py", "--data","data/dataset.yaml","--img","1024","--conf","0.001","--iou","0.7", \
                    "--weights","./E:/yolov9/runs/train-seg/gelan-c-seg-1024/weights/best.pt", \
-                   "--source",filepath,"--max-det","15"]
+                   "--source",filepath,"--max-det","15","--project","./static/runs/predict-seg/"]
             subprocess.run(cmd)
                 
-    folder_path = 'runs/predict-seg'
-    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
-    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
-    image_path = folder_path+'/'+latest_subfolder+'/'+f.filename 
-    # return render_template('index_cus.html', image_path=image_path)
-    return send_file(image_path, mimetype='image/png')
-
+            folder_path = app.config['RESULTS_FOLDER']
+            subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
+            latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
+            sam_file = [str(fi) for fi in os.listdir(os.path.join(folder_path,latest_subfolder))][1]
+            image_path = folder_path+'/'+latest_subfolder+'/'+f.filename 
+            sam_path = folder_path+'/'+latest_subfolder+'/'+sam_file
+            return render_template('index.html', image_path=image_path,image_path_2=sam_path)
+    
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov9 models")
