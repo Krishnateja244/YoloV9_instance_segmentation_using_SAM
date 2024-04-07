91bc6410a4a10ffe2cf88d74fa50e7ec07e7ffd1
Krishna Teja <krishnatejakool@gmail.com>
2024-04-07 20:30:20 +0200

    update flask application

--- app.py
+++ app.py
@@ -26,15 +26,15 @@ def predict_img():
             predict_img.imgpath = f.filename
             print("printing predict_img :::::: ", predict_img)
             
-            cmd = ["python","./segment/predict.py", "--data","data/dataset.yaml","--img","1024","--conf","0.001","--iou","0.7", \
-                   "--weights","./E:/yolov9/runs/train-seg/gelan-c-seg-1024/weights/best.pt", \
+            cmd = ["python","./segment/predict.py", "--data","./data/dataset.yaml","--img","1024","--conf","0.001","--iou","0.7", \
+                   "--weights","./runs/train-seg/gelan-c-seg-1024/weights/best.pt", \
                    "--source",filepath,"--max-det","15","--project","./static/runs/predict-seg/"]
             subprocess.run(cmd)
                 
             folder_path = app.config['RESULTS_FOLDER']
             subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
             latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
-            sam_file = [str(fi) for fi in os.listdir(os.path.join(folder_path,latest_subfolder))][1]
+            sam_file = f"{f.filename.rsplit('.',1)[0]}_sam.{f.filename.rsplit('.',1)[1]}" #[str(fi) for fi in os.listdir(os.path.join(folder_path,latest_subfolder))][1]
             image_path = folder_path+'/'+latest_subfolder+'/'+f.filename 
             sam_path = folder_path+'/'+latest_subfolder+'/'+sam_file
             return render_template('index.html', image_path=image_path,image_path_2=sam_path)
